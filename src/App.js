fb18478181f96f7c4fc104374d299e365acac462
MykaMart <myka@mykaintech.com>
2020-09-02 15:50:55 -0500

    Arrange mid-solve

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import Card from './components/Card';
+import React, { useEffect, useRef, useState }   from 'react';
+import Card                             from './components/Card';
 import './App.scss';
 
 function App() {
 
+  const [ data, setData ]             = useState(["88%","21%","12%","34%","56%","54%"]);
+  const [ cardActive, setCardActive ] = useState(false);
+  const cardDivRef                    = useRef(null);
+  const forceUpdate                   = React.useState()[1].bind(null, {})
+  const cardTitle                     = "Percent Accuracy";
+
+  useEffect(() => {
+    fetch("http://34.69.55.92/mockdata")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        console.log(result.data)
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }, [])
+
+  const selectHandler = (e) => {
+      e.preventDefault()
+      // cardDivRef.current.setAttribute("id", "card-selected-div");
+      // setCardActive(true);
+      // forceUpdate()
+  }
+
   return (
     <div className="App">
-        <div id="card-div">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="card-div" id="card-div" ref={cardDivRef}>
+          {
+            data.map((datum, index) => (
+              
+                <Card cardIndex={index} cardTitle={cardTitle} cardContent={datum} selectHandler={selectHandler} cardActive={cardActive} selected={true}/>
+            )) 
+          }
         </div>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../styles/Card.scss';
-
-const cardTitle = "Percent Accuracy";
-const cardContent = "87.0%";
 
 const Card = props => {
 
+	const [ cardActive, setCardActive]	= useState(props.cardActive)
+	const cardRef       				= useRef(null);
+	const forceUpdate 					= React.useState()[1].bind(null, {})
+	const cardTitle 					= props.cardTitle;
+	const cardContent 					= props.cardContent;
+	const cardindex     				= props.cardindex;
+	const selectHandler 				= props.selectHandler;
+
+	useEffect(() => {
+		if(cardActive === true){
+			cardRef.current.setAttribute("Class", "Card select-mode");
+		}
+	}, [])
+
+	const cardSelectHandler = (e) => {
+		e.preventDefault()
+		console.log("clicking")
+		selectHandler(e);
+		cardRef.current.setAttribute("id", "active");
+		// forceUpdate();
+	}
+
 	return(
-		<div className="Card">
+		<div key={cardindex} ref={cardRef} className="Card" onClick={(e) => cardSelectHandler(e)}>
 			<p className="card-title">{ cardTitle }</p>
 			<p className="card-content">{ cardContent }</p>
 		</div>
